1e32b7ff0bf4799d3dc9a28f3d2377c7ab2ea56a
marioonofrio <marofrio7@gmail.com>
2025-05-01 14:57:26 -0500

    updates structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './pages/Home';
 
 function App() {
   return (
       <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
           <Header />
           <main style={{ flex: 1 }}>
-              {/* Add your main content here */}
+              <Home />{/* Add your main content here */}
           </main>
           <Footer />
       </div>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css'; // Import the CSS file for styling
+
+function Home() {
+    <header>Home</header>
+    return (
+        <div className="home">
+            <h1>yo: gurt</h1>
+            <p>Salutations Fine Shyts!!</p>
+        </div>
+    )
+}
+
+export default Home;
